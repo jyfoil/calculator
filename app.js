60523fd35affca0c1858cea37dd251592d45601d
Justin Yang <yjust16@gmail.com>
2022-07-21 13:11:57 -0700

    Create basic math operator functions

---
+++ app.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+function operate(operator, num1, num2) {
+  return operator(num1, num2);
+}
