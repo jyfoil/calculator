3abc522f4c0cf4aa69802dfa810f20cc02dd015e
Justin Yang <yjust16@gmail.com>
2022-07-22 21:29:30 -0700

    Display numbers on screen

--- app.js
+++ app.js
@@ -17,3 +17,16 @@ const divide = (a, b) => {
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
+
+const display = document.querySelector(".display");
+const buttons = document.querySelector(".calc");
+
+function displayNumbers() {
+  buttons.addEventListener("click", (e) => {
+    const displayContent = document.createElement("div");
+    display.appendChild(displayContent);
+    displayContent.textContent = e.target.textContent;
+  });
+}
+
+displayNumbers();
