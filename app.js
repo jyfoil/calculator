5ade8ad22742e514a5fcf7fe920bc39f4fad51ce
Justin Yang <yjust16@gmail.com>
2022-07-25 20:13:27 -0700

    Refactor existing code

--- app.js
+++ app.js
@@ -1,3 +1,94 @@
+const display = document.querySelector(".display");
+const numberBtns = document.querySelector(".calc");
+const deleteBtn = document.querySelector(".delete");
+const clearBtn = document.querySelector(".clear");
+const operatorBtns = document.querySelector(".b-cont");
+
+let limit = 0;
+let firstNumber = [];
+let secondNumber = [];
+let temp;
+let sign;
+
+numberBtns.addEventListener("click", (e) => {
+  if (
+    e.target.matches(".numbers") &&
+    limit < 9 &&
+    typeof firstNumber !== "number"
+  ) {
+    displayNumbers(e);
+    storeNumInArray(e, firstNumber);
+  } else if (
+    e.target.matches(".numbers") &&
+    limit < 9 &&
+    typeof firstNumber === "number"
+  ) {
+    displayNumbers(e);
+    storeNumInArray(e, secondNumber);
+  }
+});
+
+operatorBtns.addEventListener("click", (e) => {
+  if (e.target.matches(".operator")) {
+    convertArrayToNum(firstNumber);
+    storeOperator(e);
+  }
+});
+
+deleteBtn.addEventListener("click", () => {
+  if (checkForValues()) {
+    deleteLastNum();
+  }
+});
+
+clearBtn.addEventListener("click", clearDisplay);
+
+function displayNumbers(e) {
+  const displayContent = document.createElement("div");
+  displayContent.classList.add("number");
+  display.appendChild(displayContent);
+  displayContent.textContent = e.target.textContent;
+  limit++;
+}
+
+function checkForValues() {
+  if (display.lastElementChild !== null) {
+    return true;
+  } else false;
+}
+
+function deleteLastNum() {
+  display.removeChild(display.lastElementChild);
+  limit--;
+  num1.pop();
+}
+
+function clearDisplay() {
+  const displayContent = document.querySelectorAll(".number");
+  displayContent.forEach((content) => {
+    display.removeChild(content);
+  });
+  num1 = [];
+  limit = 0;
+}
+
+function storeNumInArray(e, variable) {
+  variable.push(e.target.textContent);
+}
+
+function convertArrayToNum(array) {
+  firstNumber = parseInt(array.join(""));
+}
+
+function storeOperator(e) {
+  sign = e.target.classList.value;
+  const displayNumber = document.querySelectorAll(".number");
+  displayNumber.forEach((number) => {
+    display.removeChild(number);
+  });
+  limit = 0;
+}
+
 const add = (a, b) => {
   return a + b;
 };
@@ -17,66 +108,3 @@ const divide = (a, b) => {
 function operate(operator, a, b) {
   return operator(a, b);
 }
-
-const display = document.querySelector(".display");
-const buttons = document.querySelector(".calc");
-let limit = 0;
-let num1 = [];
-let num2 = [];
-let sign;
-
-function displayNumbers() {
-  buttons.addEventListener("click", (e) => {
-    if (e.target.matches(".numbers") && limit < 9) {
-      const displayContent = document.createElement("div");
-      displayContent.classList.add("number");
-      display.appendChild(displayContent);
-      displayContent.textContent = e.target.textContent;
-      limit++;
-    }
-  });
-}
-
-function deleteNum() {
-  buttons.addEventListener("click", (e) => {
-    if (e.target.matches(".delete") && limit > 0) {
-      display.removeChild(display.lastElementChild);
-      limit--;
-      num1.pop();
-    }
-  });
-}
-
-function clearDisplay() {
-  buttons.addEventListener("click", (e) => {
-    if (e.target.matches(".clear") && limit > 0) {
-      const displayContent = document.querySelectorAll(".number");
-      displayContent.forEach((content) => {
-        display.removeChild(content);
-      });
-      num1 = [];
-      limit = 0;
-    }
-  });
-}
-
-function storeFirstNumAndOperator() {
-  buttons.addEventListener("click", (e) => {
-    if (e.target.matches(".numbers") && limit < 9 && typeof num1 !== "number") {
-      num1.push(e.target.textContent);
-    } else if (e.target.matches(".operator") && typeof num1 !== "number") {
-      const displayContent = document.querySelectorAll(".number");
-      displayContent.forEach((content) => {
-        display.removeChild(content);
-      });
-      limit = 0;
-      num1 = +num1.join("");
-      sign = e.target.classList.value;
-    }
-  });
-}
-
-storeFirstNumAndOperator();
-clearDisplay();
-deleteNum();
-displayNumbers();
